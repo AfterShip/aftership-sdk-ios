79a2a5e85f5c0b37a6b029a46efc64cd64ef67bc
Chiu Lam <happylam23@gmail.com>
2016-10-12 11:53:54 +0800

    Fix import header

--- Aftership-iOS-SDK/Classes/Models/AftershipCourier.h
+++ Aftership-iOS-SDK/Classes/Models/AftershipCourier.h
@@ -4,6 +4,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "RestKit.h"
 
 @class RKObjectMapping;
 
--- Aftership-iOS-SDK/Classes/Requests/AftershipAbstractRequest.h
+++ Aftership-iOS-SDK/Classes/Requests/AftershipAbstractRequest.h
@@ -4,6 +4,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "RestKit.h"
 
 @class RKResponseDescriptor;
 @class RKObjectManager;
